a3623d51a3eeece5a0781b788269335a55c55b8b
Jacek Szambelanczyk <jacekszambelanczyk@gmail.com>
2025-05-21 16:20:51 +0200

    Adding find_edge branch

--- Project_3/graph.py
+++ Project_3/graph.py
@@ -22,4 +22,12 @@ class Graph:
     def show_table(self):
         self.table = copy.deepcopy(self.edges)
         self.table.sort(key=lambda x: x[0])
-            
+    
+    def g_print(graph, representation, n):
+        if representation == "matrix":
+            l_matrix = []
+            l_matrix.append("  | " + " ".join([str(i) for i in range(1, n+1)]))
+            l_matrix.append("--+" + "-" * (n * 2))
+
+            for row in l_matrix:
+                print(row)
