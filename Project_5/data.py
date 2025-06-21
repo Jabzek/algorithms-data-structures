4a8f3ddc93758828bdd4b7ce92f7cf0882965f2d
Jacek Szambelanczyk <jacekszambelanczyk@gmail.com>
2025-06-21 14:54:29 +0200

    Adding data save in file

---
+++ Project_5/data.py
@@ -0,0 +1,64 @@
+import csv
+
+def data_provide(filename):
+    while True:
+        try: 
+            capacity = float(input("Podaj pojemność plecaka: "))
+            if capacity <= 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Pojemność musi być liczbą większą od zera. Spróbuj ponownie.")
+    
+    while True:
+        try:
+            numberofItems = int(input("Podaj liczbę przedmiotów: "))
+            if numberofItems <= 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Liczba przedmiotów musi być większa od zera. Spróbuj ponownie.")
+
+    with open(filename, "w", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([capacity])
+        writer.writerow([numberofItems])
+    
+    for el in range(numberofItems):
+        while True:
+            try: 
+                itemWeight = float(input(f"Podaj wagę przedmiotu {el + 1}: "))
+                if itemWeight <= 0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Waga musi być większa od zera. Spróbuj ponownie.")
+
+        while True:
+            try:
+                itemValue = float(input(f"Podaj wartość przedmiotu {el + 1}: "))
+                if itemValue <= 0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Wartość musi być większa od zera. Spróbuj ponownie.")
+
+        with open(filename, "a", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow((itemValue, itemWeight))
+        
+    print(f"Dane zostały zapisane do pliku {filename}.")
+
+def data_read(filename):
+    itemValeus = []
+    itemWeights = []
+
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        
+        for el in range(2):
+            if el == 0:
+                capacity = float(reader[0])
+            else:
+                numberofItems = int(reader[1])
+        
---
+++ Project_5/main.py
@@ -0,0 +1,33 @@
+from data import data_provide
+
+
+def main():
+    while True:
+        try:
+            action = input("W jaki sposób chcesz uzyskać dane? (z pliku (File) / ręcznie (Manual)): ").strip().lower()
+            if action != "file" and action != "manual":
+                raise ValueError
+            break
+        except ValueError:
+                print("Nieprawidłowy wybór. Wybierz 'file' lub 'manual'.\n")    
+            
+    if action == "file":
+        filename = input("Podaj ścieżkę do pliku CSV: ").strip()
+
+    elif action == "manual":
+        while True:
+            try:
+                filename = input("Podaj nazwę pliku do zapisu danych (name.csv): ").strip()
+                if not filename.endswith(".csv"):
+                    raise ValueError
+                print()
+                break
+            except ValueError:
+                print("Plik musi mieć rozszerzenie .csv. Spróbuj ponownie.\n")
+        data_provide(filename)
+
+
+
+
+if __name__ == "__main__":
+    main()
