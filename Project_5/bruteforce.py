9b58302eb0b8b938fb07655fb782168e51f34cfd
Jacek Szambelanczyk <jacekszambelanczyk@gmail.com>
2025-06-21 17:54:26 +0200

    Adding bruteforce algorithm

--- Project_5/bruteforce.py
+++ Project_5/bruteforce.py
@@ -2,10 +2,18 @@ from itertools import combinations
 
 def brute_force(capacity, numberofItems, itemValues, itemWeights):
     maxValue = 0
-    bestCombination = []
     listofCombinations = []
 
     for length in range(1, numberofItems + 1):
-        listofCombinations.append(combinations(range(numberofItems), length))
+        listofCombinations.extend(list(combinations(range(numberofItems), length)))
 
-    print(listofCombinations)
+    for combination in listofCombinations:
+        totalWeight = sum(itemWeights[i] for i in combination)
+        totalValue = sum(itemValues[i] for i in combination)
+
+        if totalWeight <= capacity and totalValue > maxValue:
+            maxValue = totalValue
+            bestCombination = combination
+
+    print(f"Najlepsza kombinacja przedmiotów; {", ".join(str(i + 1) for i in bestCombination)}")
+    print(f"Łączna wartość przedmiotów: {maxValue}")
